64403605b8345787c811237c80d678ff33e761c3
Gagandeep Sharma <gagandeep.sharma88@gmail.com>
2017-04-25 14:09:13 +0530

    Directive by Hostlistener

--- src/app/shared/directives/basic-highlight.directive.ts
+++ src/app/shared/directives/basic-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive,ElementRef, OnInit,Renderer2 } from '@angular/core';
+import { Directive,ElementRef, OnInit,Renderer2, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appBasicHighlight]'
@@ -12,9 +12,22 @@ export class BasicHighlightDirective implements OnInit {
     // this.getElement.nativeElement.style.color = '#fff';
     // this.getElement.nativeElement.style.padding = '15px';
 
+    // this.renderer.setStyle(this.getElement.nativeElement, "background-color", "dodgerblue");
+    // this.renderer.setStyle(this.getElement.nativeElement, "color", "#fff");
+    // this.renderer.setStyle(this.getElement.nativeElement, "padding", "15px");
+  }
+
+  @HostListener('mouseout') mouseLeave(eventData: Event){
+    this.renderer.setStyle(this.getElement.nativeElement, "background-color", "transparent");
+    this.renderer.setStyle(this.getElement.nativeElement, "color", "#333");
+    this.renderer.setStyle(this.getElement.nativeElement, "padding", "0");
+  }
+
+  @HostListener('mousemove') onHover(eventData: Event){
     this.renderer.setStyle(this.getElement.nativeElement, "background-color", "dodgerblue");
     this.renderer.setStyle(this.getElement.nativeElement, "color", "#fff");
     this.renderer.setStyle(this.getElement.nativeElement, "padding", "15px");
   }
+
 
 }
