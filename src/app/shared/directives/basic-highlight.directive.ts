a45c569a32f0256d2bca8449a848b83094d1ad50
Gagandeep Sharma <gagandeep.sharma88@gmail.com>
2017-04-25 14:18:24 +0530

    Directive by Hostlistener/HostBinding

--- src/app/shared/directives/basic-highlight.directive.ts
+++ src/app/shared/directives/basic-highlight.directive.ts
@@ -1,13 +1,18 @@
-import { Directive,ElementRef, OnInit,Renderer2, HostListener } from '@angular/core';
+import { Directive,ElementRef, OnInit,Renderer2, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appBasicHighlight]'
 })
 export class BasicHighlightDirective implements OnInit {
 
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+  @HostBinding('style.color') color: string = '#333';
+  @HostBinding('style.padding') padding: string = '0';
+
   constructor(private getElement: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(){
+    // By Element Ref
     // this.getElement.nativeElement.style.backgroundColor = 'dodgerblue';
     // this.getElement.nativeElement.style.color = '#fff';
     // this.getElement.nativeElement.style.padding = '15px';
@@ -18,15 +23,21 @@ export class BasicHighlightDirective implements OnInit {
   }
 
   @HostListener('mouseout') mouseLeave(eventData: Event){
-    this.renderer.setStyle(this.getElement.nativeElement, "background-color", "transparent");
-    this.renderer.setStyle(this.getElement.nativeElement, "color", "#333");
-    this.renderer.setStyle(this.getElement.nativeElement, "padding", "0");
+    // this.renderer.setStyle(this.getElement.nativeElement, "background-color", "transparent");
+    // this.renderer.setStyle(this.getElement.nativeElement, "color", "#333");
+    // this.renderer.setStyle(this.getElement.nativeElement, "padding", "0");
+    this.backgroundColor = 'transparent';
+    this.color = "#333";
+    this.padding= "0";
   }
 
   @HostListener('mousemove') onHover(eventData: Event){
-    this.renderer.setStyle(this.getElement.nativeElement, "background-color", "dodgerblue");
-    this.renderer.setStyle(this.getElement.nativeElement, "color", "#fff");
-    this.renderer.setStyle(this.getElement.nativeElement, "padding", "15px");
+    // this.renderer.setStyle(this.getElement.nativeElement, "background-color", "dodgerblue");
+    // this.renderer.setStyle(this.getElement.nativeElement, "color", "#fff");
+    // this.renderer.setStyle(this.getElement.nativeElement, "padding", "15px");
+    this.backgroundColor = 'dodgerblue';
+    this.color = "#fff"
+    this.padding= "15px";
   }
 
 
