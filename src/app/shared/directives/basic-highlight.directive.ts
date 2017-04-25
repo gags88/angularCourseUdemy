417e58d050842f164ce82e20d112137c3755b64f
Gagandeep Sharma <gagandeep.sharma88@gmail.com>
2017-04-25 12:48:58 +0530

    Diretctive with Renderer

--- src/app/shared/directives/basic-highlight.directive.ts
+++ src/app/shared/directives/basic-highlight.directive.ts
@@ -1,16 +1,20 @@
-import { Directive,ElementRef } from '@angular/core';
+import { Directive,ElementRef, OnInit,Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBasicHighlight]'
 })
-export class BasicHighlightDirective {
+export class BasicHighlightDirective implements OnInit {
 
-  constructor(private getElement: ElementRef) { }
+  constructor(private getElement: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(){
-    this.getElement.nativeElement.style.backgroundColor = 'dodgerblue';
-    this.getElement.nativeElement.style.color = '#fff';
-    this.getElement.nativeElement.style.padding = '15px';
+    // this.getElement.nativeElement.style.backgroundColor = 'dodgerblue';
+    // this.getElement.nativeElement.style.color = '#fff';
+    // this.getElement.nativeElement.style.padding = '15px';
+
+    this.renderer.setStyle(this.getElement.nativeElement, "background-color", "dodgerblue");
+    this.renderer.setStyle(this.getElement.nativeElement, "color", "#fff");
+    this.renderer.setStyle(this.getElement.nativeElement, "padding", "15px");
   }
 
 }
