1764a0527d28fcc8bfbb4418754f45dbd3fb49a8
Gagandeep Sharma <gagandeep.sharma88@gmail.com>
2017-05-06 15:41:25 +0530

    use subject in shopping list service

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -4,15 +4,15 @@ import { RecipeService } from "./recipe.service";
 
 @Component({selector: 'app-recipes', templateUrl: './recipes.component.html', styleUrls: ['./recipes.component.css']})
 export class RecipesComponent implements OnInit {
-  selectedRecipe : Recipe;
+  //selectedRecipe : Recipe;
   constructor(private recipeService: RecipeService) {}
 
   ngOnInit() {
-    this.recipeService.recipeSelected.subscribe(
-      (recipe : Recipe)=> {
-        this.selectedRecipe = recipe;
-      }
-    )
+    // this.recipeService.recipeSelected.subscribe(
+    //   (recipe : Recipe)=> {
+    //     this.selectedRecipe = recipe;
+    //   }
+    // )
   }
 
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,10 +1,11 @@
-import { EventEmitter } from "@angular/core";
+//import { EventEmitter } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
+import { Subject } from "rxjs/Subject";
 
 export class ShoppingListService{
 
-    newIngredient = new EventEmitter<Ingredient[]>();
-
+    //newIngredient = new EventEmitter<Ingredient[]>();
+    newIngredient = new Subject<Ingredient[]>();
     private ingredients: Ingredient[] = [
         new Ingredient("Apples", 5),
         new Ingredient("Tomatoes", 10)
@@ -16,12 +17,13 @@ export class ShoppingListService{
 
      addIngredient(ingredient: Ingredient){
          this.ingredients.push(ingredient);
-         this.newIngredient.emit(this.ingredients.slice());
+         //this.newIngredient.emit(this.ingredients.slice());
+        this.newIngredient.next(this.ingredients.slice());
      }
 
      addMoreIngredients(ingredient: Ingredient[]){
          this.ingredients.push(...ingredient);
-         this.newIngredient.emit(this.ingredients.slice());
+         this.newIngredient.next(this.ingredients.slice());
      }
 
 }
