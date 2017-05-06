0c5cbf8b7784641ea5e28a8819d9f60ecc2b9594
Gagandeep Sharma <gagandeep.sharma88@gmail.com>
2017-05-06 22:40:02 +0530

    Handle Radio buttons in Forms and setValues programatically

--- src/app/forms/form/form.component.ts
+++ src/app/forms/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from "@angular/forms";
 
 @Component({
@@ -8,11 +8,28 @@ import { NgForm } from "@angular/forms";
 })
 export class FormComponent implements OnInit {
 
+  @ViewChild('form') theForm: NgForm;
+
   answer: string = "";
+  genders = ['Male', 'Female'];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSuggestUsername(){
+    const userName = "SuperUser";
+    this.theForm.setValue({
+      userData: {
+        username: userName,
+        email: '',
+        secret: 'pet',
+        questionAnswer: '',
+        gender: 'Male'
+      }
+    })
+
   }
 
   onFormSubmit(data: NgForm){
